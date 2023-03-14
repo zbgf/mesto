8bdb9ade4078442017c3eae25e979b4acd90ec53
Nikita <nik477271@gmail.com>
2023-03-15 01:33:39 +0300

    mesto

--- scripts/index.js
+++ scripts/index.js
@@ -1,63 +1,67 @@
 //изменение имени
-let popupEdit = document.querySelector('.popupEdit');
-let openEdit = document.querySelector('.profile__edit');
-let closeEdit = document.querySelector('.popupEdit__close');
-let profileName = document.querySelector('.profile__name');
-let profileAbout = document.querySelector('.profile__about');
-let formElement = document.querySelector('.popupEdit__form');
-let nameInput = formElement.querySelector('.popupEdit__input_type_name');
-let aboutInput = formElement.querySelector('.popupEdit__input_type_about');
+const popupEdit = document.querySelector('.popupEdit');
+const openEdit = document.querySelector('.profile__edit');
+const profileName = document.querySelector('.profile__name');
+const profileAbout = document.querySelector('.profile__about');
+const formElement = document.querySelector('.popupEdit__form');
+const nameInput = formElement.querySelector('.popupEdit__input_type_name');
+const aboutInput = formElement.querySelector('.popupEdit__input_type_about');
 
-function openPopup () {
-  popupEdit.classList.add('popupEdit_opened');
-  nameInput.value = profileName.textContent;
-  aboutInput.value = profileAbout.textContent;
+function openPopup (popup) {
+  popup.classList.add('popup_opened');
 }
 
-function closePopup () {
-  popupEdit.classList.remove('popupEdit_opened');
+function closePopup (popup) {
+  popup.classList.remove('popup_opened');
+}
+
+function openPopupEdit () {
+  openPopup(popupEdit);
+  nameInput.value = profileName.textContent;
+  aboutInput.value = profileAbout.textContent;
 }
 
 function handleFormSubmit (evt) {
   evt.preventDefault();
   profileName.textContent = nameInput.value;
   profileAbout.textContent = aboutInput.value;
-  closePopup ();
+  closePopup (popupEdit);
 }
 
-openEdit.addEventListener('click', openPopup);
-closeEdit.addEventListener('click', closePopup);
+openEdit.addEventListener('click', openPopupEdit);
 formElement.addEventListener('submit', handleFormSubmit);
 
 //изменение элементов
-let popupAdd = document.querySelector('.popupAdd');
-let formElementAdd = document.querySelector('.popupAdd__form');
-let openAdd = document.querySelector('.profile__add');
-let closeAdd = document.querySelector('.popupAdd__close');
-let placeElement = formElementAdd.querySelector('.popupAdd__input_type_place');
-let linkElement = formElementAdd.querySelector('.popupAdd__input_type_link');
-let elementList = document.querySelector('.element');
-let elementTemplate = document.querySelector('#element-template');
+const popupAdd = document.querySelector('.popupAdd');
+const formElementAdd = document.querySelector('.popupAdd__form');
+const openAdd = document.querySelector('.profile__add');
+const placeElement = formElementAdd.querySelector('.popupAdd__input_type_place');
+const linkElement = formElementAdd.querySelector('.popupAdd__input_type_link');
+const elementList = document.querySelector('.element');
+const elementTemplate = document.querySelector('#element-template');
+const popupImage = document.querySelector('.popupImage');
+const popupImagePlace = document.querySelector('.popupImage__place');
+const popupImageImage = document.querySelector('.popupImage__image');
 
-function openPopupAdd () {
-  popupAdd.classList.add('popupAdd_opened');
-}
-
-function closePopupAdd () {
-  popupAdd.classList.remove('popupAdd_opened');
-}
-
-openAdd.addEventListener('click', openPopupAdd);
-closeAdd.addEventListener('click', closePopupAdd);
+openAdd.addEventListener('click', () => openPopup(popupAdd));
 
 function create (element) {
   let copyElement = elementTemplate.content.cloneNode('true');
   copyElement.querySelector('.element__title').textContent = element.place;
   copyElement.querySelector('.element__image').src = element.link;
+  copyElement.querySelector('.element__image').alt = element.place;
   //удаление элементов
   copyElement.querySelector('.element__trash').addEventListener('click', function (elementDelete) {elementDelete.target.closest('.element__list').remove();});
   //добавление лайков
   copyElement.querySelector('.element__like').addEventListener('click', function (elementLike) {elementLike.target.classList.toggle('element__like_active');});
+  //открытие картинки
+  copyElement.querySelector('.element__image').addEventListener('click', function () {
+    openPopup(popupImage);
+    popupImagePlace.textContent = element.place;
+    popupImageImage.src = element.link;
+    popupImageImage.alt = element.place;
+  });
+
   return copyElement;
 }
 
@@ -67,7 +71,8 @@ function handleElementSubmit (evt) {
   newElement.place = placeElement.value;
   newElement.link = linkElement.value;
   elementList.prepend(create(newElement));
-  closePopupAdd();
+  evt.target.reset();
+  closePopup(popupAdd);
 }
 
 formElementAdd.addEventListener('submit', handleElementSubmit);
@@ -107,22 +112,12 @@ function initialElements (element) {
 
 initialCards.forEach(initialElements);
 
-//открытие картинки
-let popupImage = document.querySelector('.popupImage');
-let openImage = document.querySelector('.element__image');
-let closeImage = document.querySelector('.popupImage__close');
-let popupImagePlace = document.querySelector('.popupImage__place');
-let popupImageImage = document.querySelector('.popupImage__image');
-let elementTitle = document.querySelector('.element__title');
+// находим все крестики проекта по универсальному селектору
+const closeButtons = document.querySelectorAll('.popup__close');
 
-function openPopupImage () {
-  popupImage.classList.add('popupImage_opened');
-  popupImageImage.src = openImage.src;
-  popupImagePlace.textContent = elementTitle.textContent;
-}
-function closePopupImage () {
-  popupImage.classList.remove('popupImage_opened');
-}
-
-openImage.addEventListener('click', openPopupImage);
-closeImage.addEventListener('click', closePopupImage);
+closeButtons.forEach((button) => {
+  // находим 1 раз ближайший к крестику попап 
+  const popup = button.closest('.popup');
+  // устанавливаем обработчик закрытия на крестик
+  button.addEventListener('click', () => closePopup(popup));
+});
